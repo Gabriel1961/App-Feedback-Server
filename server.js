eda990555dce97f997bd0364086a1c12eca10176
Gabriel Turcin <gabriel10tm@gmail.com>
2025-03-13 22:50:00 +0200

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
